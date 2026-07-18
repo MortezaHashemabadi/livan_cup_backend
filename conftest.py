723aee6deb284ecbc115415bbb8bbc351f753476
morteza <mortezahashmabady2121@gmail.com>
2026-07-18 03:49:12 +0330

    Fix conftest to properly run migrations for in-memory test db

--- conftest.py
+++ conftest.py
@@ -10,7 +10,7 @@ import pytest
 
 
 @pytest.fixture(scope="session")
-def django_db_setup():
+def django_db_setup(django_db_blocker):
     from django.conf import settings
     settings.DATABASES["default"] = {
         "ENGINE": "django.db.backends.sqlite3",
@@ -21,9 +21,9 @@ def django_db_setup():
         "CONN_HEALTH_CHECKS": False,
         "OPTIONS": {},
         "TIME_ZONE": None,
-        "TEST": {
-            "NAME": None,
-            "MIRROR": None,
-        },
+        "TEST": {"NAME": None, "MIRROR": None},
     }
 
+    from django.test.utils import setup_databases
+    with django_db_blocker.unblock():
+        setup_databases(verbosity=1, interactive=False)
